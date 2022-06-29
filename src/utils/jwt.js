3de2d8548e9db777927148c4ff50818ad54f12f7
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-29 19:31:50 -0300

    autenticacao do token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -12,4 +12,20 @@ const generateJWTToken = (email) => {
   return token;
 };
 
-module.exports = generateJWTToken;
+const authenticateToken = async (token) => {
+  if (!token) {
+    const error = { status: 401, message: 'Token not found' };
+    throw error;
+  }
+
+  try {
+    const validate = jwt.verify(token, TOKEN_SECRET);
+    return validate;
+  } catch (error) {
+    console.log(error);
+    const error2 = { status: 401, message: 'Expired or invalid token' };
+    throw error2;
+  }
+};
+
+module.exports = { generateJWTToken, authenticateToken };
