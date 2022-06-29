f47a18f322948fd370c3606b317bed715e2659a4
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-29 16:22:25 -0300

    consertando os erros da pagina

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,13 +1,15 @@
 const jwt = require('jsonwebtoken');
 
-const { TOKEN_SECRET } = process.env.TOKEN_SECRET;
+const TOKEN_SECRET = process.env.JWT_SECRET;
 
 const jwtConfig = {
   expireIn: '48h',
   algorithm: 'HS256',
 };
 
-const generateJWTToken = (payload) => {
-  jwt.sign(payload, TOKEN_SECRET, jwtConfig);
+const generateJWTToken = (email) => {
+  const token = jwt.sign({ email }, TOKEN_SECRET, jwtConfig);
+  return token;
 };
-module.exports = { generateJWTToken };
+
+module.exports = generateJWTToken;
