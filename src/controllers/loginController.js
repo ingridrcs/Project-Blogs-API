fbc765ae469d7792b8f58ef2626cdb1284d051de
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-30 13:54:24 -0300

    colocacao do try/catch no controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,8 +1,13 @@
 const loginService = require('../services/loginServices');
 
 const loginUser = async (req, res) => {
-  const token = await loginService.authenticate(req.body);
-  console.log(token);
-  res.status(200).json(token);
+    try {
+   const token = await loginService.authenticate(req.body);
+   console.log('req', req.body);
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: error.message });
+  }
 };
 module.exports = { loginUser };
--- src/services/loginServices.js
+++ src/services/loginServices.js
@@ -1,4 +1,4 @@
-const user = require('../database/models/user');
+const { User } = require('../database/models'); 
 const { generateJWTToken } = require('../utils/jwt');
 
 const authenticate = async ({ email, password }) => {
@@ -6,16 +6,18 @@ const authenticate = async ({ email, password }) => {
     const error = { status: 400, message: 'Some required fields are missing' };
     throw error;
   }
-  const userLogin = await user.findOne({
-    where: { email, password },
+
+  const userLogin = await User.findOne({
+    where: { email },
   });
 
   if (!userLogin) {            
     const error = { status: 400, message: 'Invalid fields' };
     throw error;
   }
-  const token = generateJWTToken(email);
-  return { token };
+  const tokenUser = generateJWTToken(email);
+  // console.log('oi', token);
+  return tokenUser;
 };
 
 module.exports = { authenticate };
