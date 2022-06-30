4643545d1da68de4b2fb5af7d4766edd608012bd
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-30 13:53:35 -0300

    mudanca na chamada da rota

--- src/api.js
+++ src/api.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/error');
 
 const loginRoute = require('./routes/loginRoute');
-
+const userRoute = require('./routes/userRoute');
 // ...
 
 const app = express();
 
 app.use(express.json());
-app.use('/login', loginRoute);
+app.use('/', loginRoute);
+app.use('/', userRoute);
 
+app.use(errorMiddleware);
 // ...
 
 // É importante exportar a constante `app`,
