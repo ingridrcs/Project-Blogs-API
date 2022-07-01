162bccc9c57924a70e47910cdd77be26f4fb278b
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-01 17:16:24 -0300

    criando a rota

--- src/routes/categoriesRoute.js
+++ src/routes/categoriesRoute.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const categoriesController = require('../controllers/categoriesController');
 const tokenMiddle = require('../middlewares/tokenMiddleware');
 
-const categoryMiddle = require('../middlewares/categoryMiddleware');
+const { categoryValidation } = require('../middlewares/categoryMiddleware');
 
-router.post('/categories', categoryMiddle, tokenMiddle, categoriesController.addCategory);
-
+router.post('/categories', tokenMiddle, categoryValidation, categoriesController.addCategory);
+router.get('/categories', tokenMiddle, categoriesController.getCategories);
 module.exports = router;
