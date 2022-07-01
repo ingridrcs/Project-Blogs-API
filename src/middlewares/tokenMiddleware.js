5c107d741cb5f9cff34deb036f0ff1164d9450e2
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-30 23:41:38 -0300

    colocação do try/catch para o código rodar

--- src/middlewares/tokenMiddleware.js
+++ src/middlewares/tokenMiddleware.js
@@ -1,13 +1,18 @@
 const { authenticateToken } = require('../utils/jwt');
 
 const authenticateMiddleware = async (req, res, next) => {
+  try {
 const token = req.headers.authorization;
 const user = await authenticateToken(token);
 if (!user) {
-  const error = { status: 400, message: 'Token not found' };
-    throw error;
+  return res.status(400).json({ message: 'Token not found' });
 }
+  
 res.locals.user = user;
 next();
+} catch (error) {
+    console.log(error);
+    return res.status(401).json({ message: error.message });
+  }
 };
 module.exports = authenticateMiddleware;
