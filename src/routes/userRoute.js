155c79e8bc1ee222e20afd248964373dd4b975bb
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-30 21:49:56 -0300

    colocação do middleware na rota

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const userController = require('../controllers/userController');
+// const tokenMiddle = require('../middlewares/tokenMiddleware');
+const userMiddle = require('../middlewares/userMiddleware');
 
-router.post('/user', userController.addUser);
+router
+.post('/user', userMiddle.emailValidation, userMiddle.passwordValidation, userController.addUser);
 
 module.exports = router;
