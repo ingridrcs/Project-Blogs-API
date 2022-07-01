9cab2e5b6ac4b1c7c8b4db806255284e04a4e358
Ingrid_Ramos <ingridrcs@gmail.com>
2022-06-30 21:48:53 -0300

    colocação do try e catch para a funçao funcionar

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,12 @@
 const userService = require('../services/userServices');
 
 const addUser = async (req, res) => {
+      try {
   const token = await userService.addUser(req.body);
-  res.status(200).json(token);
+  res.status(201).json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(409).json({ message: error.message });
+  }
 };
 module.exports = { addUser };
