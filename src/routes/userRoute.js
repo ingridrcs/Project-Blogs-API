ab8adbf730a0ffcc8bed094ed4f459d76354eccc
Ingrid_Ramos <ingridrcs@gmail.com>
2022-07-01 00:01:28 -0300

    criacao da rota user/:id

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -8,4 +8,5 @@ const userMiddle = require('../middlewares/userMiddleware');
 router
 .post('/user', userMiddle.emailValidation, userMiddle.passwordValidation, userController.addUser);
 router.get('/user', tokenMiddle, userController.getUser);
+router.get('/user/:id', tokenMiddle, userController.getUserId);
 module.exports = router;
